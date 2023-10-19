d543c767680947fd8cd433723e83589f01d28843
Alizq <132275286+Alizq@users.noreply.github.com>
2023-10-19 19:14:43 +0900

    message変更

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const client = new Client({
 });
 
 client.on('message', (message) => {
-  if (message.content === '!ping') {
-    // メッセージが '!ping' の場合、Pingを計測
+  if (message.content === 'v!ping') {
     const ping = Date.now() - message.createdTimestamp;
     message.reply(`Ping: ${ping}ms`);
   }
 });
+
+
 
 client.once('ready', () => {
   console.log(`Online ${client.user.tag}`);
