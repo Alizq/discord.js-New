5e4e6943e10d6c7d533c490a7469675fb098fb40
Alizq <132275286+Alizq@users.noreply.github.com>
2023-10-19 19:12:20 +0900

    ping取得

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
 
+client.on('message', (message) => {
+  if (message.content === '!ping') {
+    // メッセージが '!ping' の場合、Pingを計測
+    const ping = Date.now() - message.createdTimestamp;
+    message.reply(`Ping: ${ping}ms`);
+  }
+});
+
 client.once('ready', () => {
   console.log(`Online ${client.user.tag}`);
 });
