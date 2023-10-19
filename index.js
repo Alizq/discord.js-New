65e7df8c001aac9c7fa36b172b336d64aeb45b1f
Alizq <132275286+Alizq@users.noreply.github.com>
2023-10-19 19:06:50 +0900

    起動したときのログ

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
 
+client.once('ready', () => {
+  console.log(`Online ${client.user.tag}`);
+});
+
 client.login(process.env.token);
